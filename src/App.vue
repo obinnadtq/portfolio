bb47a37bdc6be0ba3afc2beec11c6d6c4fefc3bd
obinnadtq <oisiwekpeni@gmail.com>
2020-04-03 03:25:31 +0200

    styled the header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <Header></Header>
     <Summary></Summary>
     <about-me></about-me>
@@ -28,12 +28,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,63 @@
 <template>
   <div class="header">
-    <ul>
-      <li><a href="#home">Home </a></li>
-      <li><a href="#about">About Me </a></li>
-      <li><a href="#portfolio">Portfolio </a></li>
-      <li><a href="#contact">Contact </a></li>
-      <li><a href="#blog"> Blog </a></li>
-    </ul> 
-    
+    <ul class="nav-bar">
+      <li>
+        <a href="#home">Home</a>
+      </li>
+      <li>
+        <a href="#about">About Me</a>
+      </li>
+      <li>
+        <a href="#portfolio">Portfolio</a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
+      <li>
+        <a href="#blog">Blog</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-  props: {
-    
-  }
-}
+  name: "Header",
+  props: {}
+};
 </script>
 
 <style scoped>
-ul{
-  list-style-type: none;
+.header {
+  background-color: #f8f8f8;
+  height: 70px;
+  font-family: sans-serif;
+  text-align: right;
 }
 
-li{
+ul {
+  list-style-type: none;
+  padding: 25px 400px 0px 0px;
+  top: 0%;
+}
+
+li {
   display: inline;
 }
+
+li a {
+  padding-left: 50px;
+  color: black;
+  text-decoration: none;
+}
+a:link,
+a:visited {
+  color: black;
+}
+a:hover,
+a:active {
+  color: #feb633;
+}
+
+
 </style>
